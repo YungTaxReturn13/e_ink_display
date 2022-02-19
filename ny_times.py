92634a819efa97f8a381f5d7425c2597f2c24d04
Justin Pak <justinpak13@gmail.com>
2022-02-19 16:22:26 -0500

    rotating image

--- ny_times.py
+++ ny_times.py
@@ -1,5 +1,7 @@
 # import module
+import imp
 from pdf2image import convert_from_path
+from PIL import Image
 
 
 # Store Pdf with convert_from_path function
@@ -10,3 +12,6 @@ for i in range(len(images)):
     # Save pages as images in the pdf
     images[i].save("page" + str(i) + ".jpg", "JPEG")
 
+with Image.open("page.jpg") as im:
+    im.rotate(90)
+    im.save("page.jpg")
